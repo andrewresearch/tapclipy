6fba15976b7579491c42a2715ddc9ed8a063e6b0
Andrew Gibson <andrew@andrewresearch.net>
2018-04-11 17:46:57 +1000

    tapclipy-1 Need to check URL on startup

--- tapclipy.py
+++ tapclipy.py
@@ -1,5 +1,7 @@
 
 class TapCliPy:
-    def __init__(self,url='http://localhost:9000/graphql'):
+    def __init__(self,url='http://localhost:9000'):
         self.url = url
 
+    def checkConnection(self):
+        return False
