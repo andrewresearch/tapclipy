b664ccd286602a878b6e80dd83bd67ad9d202872
Andrew Gibson <andrew@andrewresearch.net>
2018-04-12 11:51:51 +1000

    tapclipy-1 Need to check URL on startup

--- TapCliPy/TapConnect.py
+++ TapCliPy/TapConnect.py
@@ -43,10 +43,10 @@ class Connect:
         return self.__tap_connect(analyse_query)
 
     def __tap_connect(self, query):
-        json_header = {'Content-Type': 'application/json'}
-        tap_request = request.Request(self.__full_url, data=query.encode('utf8'), headers=json_header)
         try:
-            tap_response = request.urlopen(tap_request)„
+            json_header = {'Content-Type': 'application/json'}
+            tap_request = request.Request(self.__full_url, data=query.encode('utf8'), headers=json_header)
+            tap_response = request.urlopen(tap_request)
             body = tap_response.read().decode('utf8')
             self.__can_connect = True
             return json.loads(body)
